6374ca3631db0df349230c48aea049ea83257ee0
Zakaria Ajaddou <54480895+MinProGamer@users.noreply.github.com>
2024-09-01 23:14:43 +0200

    First launch

---
+++ functions/function.js
@@ -0,0 +1,58 @@
+const jwt = require("jsonwebtoken");
+const pool = require("./db");  // connection
+
+const dotenv = require("dotenv");
+dotenv.config(); // Load environment variables
+
+const jwtSecret = process.env.JWT_SECRET;
+
+function SQL(query, params = []){
+    
+    return new Promise( (resolve, reject) => {
+            pool.query(query, params,(err, results) => {
+                err ? reject(err) : resolve(results);
+            });
+    });
+}
+
+function NewToken(pyload){
+    const token = jwt.sign(pyload, jwtSecret);
+    return token
+}
+
+function VerifyUser(token) {
+
+    return new Promise((resolve, reject) => {
+
+        jwt.verify(token, jwtSecret, async (err, decoded) => { 
+
+            if (err) {
+                reject(" Invalid token ");
+                return;
+            }
+
+            const { email, pass } = decoded;
+
+            try {
+
+                const [user_data] = await SQL("SELECT * FROM users WHERE email = ? AND pass = ?", [email, pass])
+
+                if (user_data) 
+                    resolve(user_data);
+
+                else
+                    reject("Invalid User");
+
+            } catch (error) {
+                reject(error);
+            }
+
+        });
+    });
+}
+
+module.exports = {
+    SQL,
+    NewToken,
+    VerifyUser
+};
